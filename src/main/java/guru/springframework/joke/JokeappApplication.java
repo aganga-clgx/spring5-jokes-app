ce9394a85ce2bec246b52dc63088afd30a0c599d
aganga-clgx <47671120+aganga-clgx@users.noreply.github.com>
2019-07-10 17:22:19 -0400

    Update JokeappApplication.java
    test 2nd commit

--- src/main/java/guru/springframework/joke/JokeappApplication.java
+++ src/main/java/guru/springframework/joke/JokeappApplication.java
@@ -2,7 +2,7 @@ package guru.springframework.joke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//test
+//test 2nd commit
 @SpringBootApplication
 public class JokeappApplication {
 
